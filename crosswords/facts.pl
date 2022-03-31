f01e2b05a0280d4cdc84be0bc791ca232feae69e
Mahmoud Mohammed Ali <70591959+MahmoudMuhammedAli@users.noreply.github.com>
2022-03-31 03:36:53 +0200

    Cross words example

--- crosswords/facts.pl
+++ crosswords/facts.pl
@@ -1,22 +1,29 @@
-word( d,o,g).
-word( f,o,u,r).
-word( b,a,k,e,r).
-word( p,r,o,l,o,g).
-word( r,u,n).
-word( 1,o,s,t).
-word( f,o,r,u,m).
-word( v,a,n,i,s,h).
-word( t,o,p).
-word( m,e,s,$).
-word( g,r,e,e,n).
-word( w,o,n,d,e,r).
-word( f,i,v,e).
-word( u,n,i,t).
-word( s,u,p,e,r).
-word( y,e,l,l,o,w).
+# l1  | l2  | l3  | l4  | l5  | ---
+# l6  | --- | l7  | --- | l8  | ---
+# l9  | l10 | l11 | l12 | l13 | l14
+# l15 | --- | --- | --- | l16 | ---
+
+
+
+word(d,o,g).
+word(f,o,u,r).
+word(b,a,k,e,r).
+word(p,r,o,l,o,g).
+word(r,u,n).
+word(1,o,s,t).
+word(f,o,r,u,m).
+word(v,a,n,i,s,h).
+word(t,o,p).
+word(m,e,s,s).
+word(g,r,e,e,n).
+word(w,o,n,d,e,r).
+word(f,i,v,e).
+word(u,n,i,t).
+word(s,u,p,e,r).
+word(y,e,l,l,o,w).
 solution( Ll,L2,L3,L4,L5,L6,L7,L8,L9,L10,L11,L12,L13,L14,L15,L16) :-
- word( L1,L2,L3,L4,L5),
- word( L9,L10,L11,L12,L13,L14),
- word( L1,L6,L9,L15),
- word( L3,L7,L11),
- word( L5,L8,L13,L16).
+ word(L1,L2,L3,L4,L5),
+ word(L9,L10,L11,L12,L13,L14),
+ word(L1,L6,L9,L15),
+ word(L3,L7,L11),
+ word(L5,L8,L13,L16).
